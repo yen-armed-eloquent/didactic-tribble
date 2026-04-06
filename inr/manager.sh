a151148f9d19e8ef6cf37d7aa48585c2357cde2f
yen-armed-eloquent <yen-armed-eloquent@outlook.com>
2026-04-06 19:57:57 +0500

    Update manager.sh

--- inr/manager.sh
+++ inr/manager.sh
@@ -10,19 +10,17 @@ function fetch_and_merge() {
     if ls $BATCH_PATH/Dataset_Pod_*.json 1> /dev/null 2>&1; then
         jq -s 'flatten' $BATCH_PATH/Dataset_Pod_*.json > $MASTER_FILE
         TOTAL_POSTS=$(jq 'length' $MASTER_FILE)
-        echo "✅ Master File Updated: $MASTER_FILE | Total: $TOTAL_POSTS Posts."
-        echo "--------------------------------------------------"
-        jq -r '.[] | "Post: \(.shortcode) | Comments: \(.commentsRawData | length)"' $MASTER_FILE
-        echo "--------------------------------------------------"
+        echo "✅ Master File Created: $MASTER_FILE | Total: $TOTAL_POSTS Posts."
     else
-        echo "⚠️ Abhi tak koi data files create nahi huin."
+        echo "⚠️ No data files found."
     fi
 }
 
 if [ "$COMMAND" == "finalize" ]; then
     fetch_and_merge
     echo "☁️ Uploading entire Batch folder to MEGA..."
-    rclone config create vfx mega user "enragedoatmeal6@sharebot.net" pass "zEUj2zsBGHxy" --non-interactive > /dev/null 2>&1
+    # Secrets ka use
+    rclone config create vfx mega user "$MEGA_USER" pass "$MEGA_PASS" --non-interactive > /dev/null 2>&1
     rclone copy $BATCH_PATH vfx:Instagram_Data/Batch_${BATCH_NAME}/ --progress
     echo "✅ MEGA UPLOAD DONE!"
 fi
